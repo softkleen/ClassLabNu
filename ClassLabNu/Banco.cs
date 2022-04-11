8198173f7f06cfd53e075518b20866619f253013
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2022-04-11 11:55:22 -0300

    Classe Banco

---
+++ ClassLabNu/Banco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ClassLabNu
+{
+    public static class Banco
+    {
+        public static MySqlCommand Abrir() 
+        { 
+            MySqlCommand cmd = new MySqlCommand();
+            string strcon = @"";
+            MySqlConnection cn = new MySqlConnection(strcon);
+            cn.Open();
+            cmd.Connection = cn;
+            return cmd;
+        } 
+    }
+}
