c05e21f2b4fff81889fcb6dc43dbf7d6f34ff156
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2022-04-20 13:54:42 -0300

    modelagem

---
+++ Procedures_comercialdb0191.sql
@@ -0,0 +1,45 @@
+-- Criação de Procedures 
+use comercialdb0191
+
+-- Procedure de insert cliente
+-- drop PROCEDURE sp_cliente_inserir;
+delimiter |
+CREATE PROCEDURE sp_cliente_inserir(
+_nome varchar(60), 
+_cpf varchar(11),
+_email varchar(60)
+)
+BEGIN
+	insert into clientes (nome,cpf, email, datacad, ativo)
+    values (_nome, _cpf,_email, default, default);
+    select * from cliente where id = (select @@identity);    
+END
+
+ -- Procedure Atualização clientetipos
+ -- drop PROCEDURE sp_cliente_alterar;
+delimiter |
+CREATE PROCEDURE sp_cliente_alterar(
+_id int ,
+_nome varchar(60), 	
+_email varchar(60)
+)
+BEGIN
+	update clientes set nome = _nome, email = _email where idcli = _id;
+END
+|
+
+-- Usuários
+delimiter |
+CREATE PROCEDURE sp_cliente_usuario(
+_nome varchar(60), 
+_email varchar(60),
+_senha varchar(32),
+_idnv int
+)
+BEGIN
+insert usuarios (nome, email, senha,idnv_user,ativo) 
+values (_nome, _email, md5(_senha), _idnv, default);
+    select * from usuarios where iduser = (select @@identity);    
+END
+|
+
