d3e18af932dc96d9a7fad4db739399e9342af38d
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2022-04-25 09:46:28 -0300

    comando sql niveis

---
+++ comandos_Sql_aula.sql
@@ -0,0 +1,59 @@
+use comercialdb0191;
+
+insert clientes (nome, cpf, email, datacad, ativo) values('Well Santos','11122222475','well@senac.corp',default, default);
+insert clientes (nome, cpf, email, datacad, ativo) values('Adriana Santos','11133322475','adriana@senac.corp',default, default);
+insert clientes (nome, cpf, email, datacad, ativo) values('Bruna Paiva','99933322475','bruna@senac.corp',default, default);
+
+-- Store Procedure
+
+
+select * from clientes;
+
+delimiter |
+CREATE PROCEDURE sp_cliente_inserir(
+_nome varchar(60), 
+_cpf varchar(11),
+_email varchar(60)
+)
+BEGIN
+	insert into clientes (nome,cpf, email, datacad, ativo)
+    values (_nome, _cpf,_email, default, default);
+    select * from clientes where idcli = (select @@identity);    
+END
+|
+
+
+
+update clientes set ativo = 0 where idcli = 12;
+
+select nome as 'seu lindo', email from clientes where idcli = 5;
+
+
+select * from clientes;
+delete from clientes where idcli = 11;
+select * from clientes;
+
+select * from clientes order by nome;
+
+insert usuarios (nome, email, senha,nivel,ativo) 
+values ('Jose', 'jose@senac.corp', md5('123456'), default, default);
+select * from usuarios;
+
+
+delimiter |
+CREATE PROCEDURE sp_cliente_usuario(
+_nome varchar(60), 
+_email varchar(60),
+_senha varchar(32),
+_idnv int
+)
+BEGIN
+insert usuarios (nome, email, senha,idnv_user,ativo) 
+values (_nome, _email, md5(_senha), _idnv, default);
+    select * from usuarios where iduser = (select @@identity);    
+END
+|
+
+
+select * from niveis;
+insert niveis values(0,'Atendente', 'cx'),(0,'Caixa', 'cx'),(0,'Gerente', 'cx');
